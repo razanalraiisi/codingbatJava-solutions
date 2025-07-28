8f85ef5562740ccf9a83c2e0101e88fcc781e868
Razan Al Raiisi <razanraiisi@gmail.com>
2025-07-28 20:17:50 +0400

    solution for logic 1 squirrel play

---
+++ logic1/squirrelPlay.java
@@ -0,0 +1,9 @@
+public boolean squirrelPlay(int temp, boolean isSummer) {
+    if((temp>=60 && temp<=90) && !isSummer){
+        return true;
+    }else if((temp>=60 && temp<=100) && isSummer){
+        return true;
+    }else{
+        return false;
+    }
+}
