2682c9f332bd125e0c86c7c517da2e8f096210d4
Razan Al Raiisi <razanraiisi@gmail.com>
2025-07-28 23:03:34 +0400

    solution for logic 1 answer cell

---
+++ logic1/answeCell.java
@@ -0,0 +1,9 @@
+public boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
+    if(isAsleep){
+        return false;
+    }else if(isMorning && !isMom){
+        return false;
+    }else{
+        return true;
+    }
+}
