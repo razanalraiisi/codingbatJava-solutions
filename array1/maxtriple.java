d09236c5c84e721e1ecba009ca7bf6577062c35f
Razan Al Raiisi <razanraiisi@gmail.com>
2025-07-29 13:47:29 +0400

    solution for array1 max triple

---
+++ array1/maxtriple.java
@@ -0,0 +1,13 @@
+public int maxTriple(int[] nums) {
+    int first = nums[0];
+    int middle = nums[nums.length / 2];
+    int last = nums[nums.length - 1];
+
+    if (first >= middle && first >= last) {
+        return first;
+    } else if (middle >= first && middle >= last) {
+        return middle;
+    } else {
+        return last;
+    }
+}
