4ecefa49dc1b1fc617281639780d1483994d418a
Razan Al Raiisi <razanraiisi@gmail.com>
2025-08-03 22:02:58 +0400

    solution for recursion 1 parenBit

---
+++ recursion1/parenBit.java
@@ -0,0 +1,10 @@
+public String parenBit(String str) {
+    if (str.charAt(0) == '(' && str.charAt(str.length() - 1) == ')') {
+        return str;
+    } else if (str.charAt(0) != '(') {
+        return parenBit(str.substring(1));
+    } else if (str.charAt(str.length() - 1) != ')') {
+        return parenBit(str.substring(0, str.length() - 1));
+    }
+    return "";
+}
