f793d4330b9d13514d88ee56c2af6ebeeccca6a0
Razan Al Raiisi <razanraiisi@gmail.com>
2025-08-04 22:51:34 +0400

    solution for array2 fizzbuzz

---
+++ array2/fizzbuzz.java
@@ -0,0 +1,16 @@
+public String[] fizzBuzz(int start, int end) {
+    String[] result = new String[end - start];
+    for (int i = 0; i < result.length; i++) {
+        int num = start + i;
+        if (num % 15 == 0) {
+            result[i] = "FizzBuzz";
+        } else if (num % 3 == 0) {
+            result[i] = "Fizz";
+        } else if (num % 5 == 0) {
+            result[i] = "Buzz";
+        } else {
+            result[i] = String.valueOf(num);
+        }
+    }
+    return result;
+}
