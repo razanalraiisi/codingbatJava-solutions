841dd4fffb561325ea7e7ccbfdd69f6dd690f2ea
Razan Al Raiisi <razanraiisi@gmail.com>
2025-08-04 21:44:59 +0400

    solution for array2 sum67

---
+++ array2/sum67.java
@@ -0,0 +1,15 @@
+public int sum67(int[] nums) {
+    int sum = 0;
+
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[i] == 6) {
+            while (i < nums.length && nums[i] != 7) {
+                i++;
+            }
+        } else {
+            sum += nums[i];
+        }
+    }
+
+    return sum;
+}
