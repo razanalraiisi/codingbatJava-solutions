daf578086b3c86791b3d98ed9ab0dd2154613eca
Razan Al Raiisi <razanraiisi@gmail.com>
2025-08-04 14:05:03 +0400

    solution for string2 repeat seperator

---
+++ string2/repeatSeperator.java
@@ -0,0 +1,10 @@
+public String repeatSeparator(String word, String sep, int count) {
+    if (count == 0) {
+        return "";
+    }
+    String result = word;
+    for (int i = 1; i < count; i++) {
+        result += sep+word;
+    }
+    return result;
+}
