41effc8dbcc9fe2128b6beaaa5bfac90dc244d07
Razan Al Raiisi <razanraiisi@gmail.com>
2025-08-05 12:49:38 +0400

    solution for logic2 black jack

---
+++ logic2/blackJack.java
@@ -0,0 +1,21 @@
+public int blackjack(int a, int b) {
+    if (a > 21 && b > 21) {
+        return 0;
+    }
+
+    if (a > 21) {
+        return b;
+    }
+    if (b > 21) {
+        return a;
+    }
+
+
+    if (Math.abs(21 - a) < Math.abs(21 - b)) {
+        return a;
+    } else if (Math.abs(21 - b) < Math.abs(21 - a)) {
+        return b;
+    } else {
+        return a;
+    }
+}
