b8f0dc99da5c3f024d0945f02de37927c415324a
Razan Al Raiisi <razanraiisi@gmail.com>
2025-08-05 12:59:37 +0400

    solution for logic2 evenly spaced

---
+++ logic2/evenlySpaced.java
@@ -0,0 +1,6 @@
+public boolean evenlySpaced(int a, int b, int c) {
+    int small = Math.min(a, Math.min(b, c));
+    int large = Math.max(a, Math.max(b, c));
+    int medium = a + b + c - small - large;
+    return (medium - small) == (large - medium);
+}
