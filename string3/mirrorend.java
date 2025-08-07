a7170b18848de724aaa3c01498789eba0d7eb4de
Razan Al Raiisi <razanraiisi@gmail.com>
2025-08-07 12:29:30 +0400

    solution for string3 mirror end

---
+++ string3/mirrorend.java
@@ -0,0 +1,13 @@
+public String mirrorEnds(String string) {
+    StringBuilder mirror = new StringBuilder();
+
+    for (int i = 0; i < string.length(); i++) {
+        if (string.charAt(i) == string.charAt(string.length() - 1 - i)) {
+            mirror.append(string.charAt(i));
+        } else {
+            break;
+        }
+    }
+
+    return mirror.toString();
+}
